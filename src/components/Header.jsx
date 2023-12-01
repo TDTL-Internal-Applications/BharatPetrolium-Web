e47dd3e9cf43a94670de38e750fe35baa9944b1a
surajtdtl <suraj.tapase@tdtl.world>
2023-12-01 18:35:17 +0530

    Updated RD Form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,26 @@
 import React from "react";
-import { FaBell, FaRegCircle, FaGlobe } from "react-icons/fa";
-// import { ImSearch } from "react-icons/im";
+import { FaBell } from "react-icons/fa";
+import { IoPersonSharp } from "react-icons/io5";
 
 export default function Header() {
 
-  const adminName = localStorage.getItem("membersData");
+  const adminName = localStorage.getItem("email");
 
   return (
     <div>
       {/* Search */}
       <div className="container d-flex w-100">
-        <div className="row w-100 py-1 px-3 d-flex justify-content-center align-items-center">
-          {/* Left side (Search) */}
-          <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12">
-            <div className="row">
-               {/* <ImSearch
-                style={{
-                  position: "absolute",
-                  right: "10px",
-                  top: "50%",
-                  transform: "translateY(-50%)",
-                }}
-              /> */}
-              <input
-                type="search"
-                placeholder="Search Your Needs"
-                style={{
-                  width: "100%",
-                  padding: "1rem",
-                  borderRadius: "15px",
-                  outline: "none",
-                  border: "none",
-                  backgroundColor: "whitesmoke",
-                }}
-              />
-             
-            </div>
-          </div>
-
+        <div className="row w-100 d-flex header-content">
           {/* Right side (Profile content) */}
-          <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 d-flex justify-content-center pb-0 pt-0">
-            <div className="nav-right d-flex align-items-center">
-              <div className="icon-container d-flex align-items-center p-3">
+          <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 pb-0 pt-0 ">
+            <div className="nav-right d-flex align-items-center justify-content-end">
+              <div className="icon-container d-flex align-items-center pe-2">
                 <FaBell className="icon" />
               </div>
-              <div className="icon-container d-flex align-items-center p-3">
+              <div className="icon-container d-flex align-items-center ps-2">
                 {adminName}
-                <FaRegCircle className="icon" />
+                <IoPersonSharp className="icon" />
               </div>
-              {/* <div className="icon-container d-flex align-items-center p-3">
-                English
-                <FaGlobe className="icon" />
-              </div> */}
             </div>
           </div>
         </div>
